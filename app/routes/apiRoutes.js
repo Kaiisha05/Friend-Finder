2ef722c0bbcd9a4d169f662f4b8eae276eb2dec5
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-09 15:41:33 -0500

    GET route for survey works

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,31 +1,31 @@
-// //Load Data
-// // ======================================================
-// var friendsData = require("../data/friends")
+//Load Data
+// ======================================================
+var friendsData = require("../data/friends")
 
 
-// //GET Routing
-// // ======================================================
-// module.exports = function (app) {
-//   app.get("/api/friends", function (req, res) {
-//     res.json(friendsData);
-//   });
-//   //POST Routing
-//   // ======================================================
-//   //Display matched friend -- will hanlde the compatibiilty logic
-//   app.post("api/friends", function (req, res) {
-//     var newFriend = req.body;
+//GET Routing
+// ======================================================
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
+  //POST Routing
+  // ======================================================
+  //Display matched friend -- will hanlde the compatibiilty logic
+  app.post("api/friends", function (req, res) {
+    var newFriend = req.body;
 
-//     //Using a RegEx Pattern to remove spaces from newFriend
+    //Using a RegEx Pattern to remove spaces from newFriend
 
-//     // newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    // newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
-//     console.log(newFriend);
+    console.log(newFriend);
 
-//     friends.push(newFriend);
+    friends.push(newFriend);
 
-//     res.json(newFriend);
-//   });
-// };
+    res.json(newFriend);
+  });
+};
 
 
 
