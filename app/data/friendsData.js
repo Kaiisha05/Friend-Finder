77361a6d731d3a008f29e696e2e5b2b425abb3d5
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-19 15:06:12 -0500

    establishing heroku deployment

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,35 +1,35 @@
 var friendsArray = [
     {
     yourName: "Mickey Mouse",
-    yourPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fd%2Fd4%2FMickey_Mouse.png%2F220px-Mickey_Mouse.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMickey_Mouse&docid=o8fKNZd6urBFOM&tbnid=cfGkw9uCLWAi0M%3A&vet=10ahUKEwj3_pi8xuPmAhWSrVkKHVmBDB8QMwh0KAAwAA..i&w=220&h=220&bih=731&biw=1440&q=mickey%20mouse&ved=0ahUKEwj3_pi8xuPmAhWSrVkKHVmBDB8QMwh0KAAwAA&iact=mrc&uact=8"
-    // scores:[
-    //     1,
-    //     5,
-    //     3,
-    //     2,
-    //     4,
-    //     2,
-    //     1,
-    //     5, 
-    //     3,
-    //     5
-    // ]
+    yourPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fd%2Fd4%2FMickey_Mouse.png%2F220px-Mickey_Mouse.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMickey_Mouse&docid=o8fKNZd6urBFOM&tbnid=cfGkw9uCLWAi0M%3A&vet=10ahUKEwj3_pi8xuPmAhWSrVkKHVmBDB8QMwh0KAAwAA..i&w=220&h=220&bih=731&biw=1440&q=mickey%20mouse&ved=0ahUKEwj3_pi8xuPmAhWSrVkKHVmBDB8QMwh0KAAwAA&iact=mrc&uact=8",
+    scores:[
+        1,
+        5,
+        3,
+        2,
+        4,
+        2,
+        1,
+        5, 
+        3,
+        5
+    ]
     },
     {
       yourName: "Minnie Mouse",
-      yourPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.homedepot-static.com%2FproductImages%2Fd8a8d24a-7866-465c-829e-194c9ab87a24%2Fsvn%2Fblue-roommates-wall-decals-rmk1509gm-64_1000.jpg&imgrefurl=https%3A%2F%2Fwww.homedepot.com%2Fp%2FRoomMates-5-in-x-19-in-Mickey-and-Friends-Minnie-Mouse-Peel-and-Stick-Giant-Wall-Decal-8-Piece-RMK1509GM%2F202984854&docid=Ki3lxYdcYd4oaM&tbnid=YdK4YuW-kt-v0M%3A&vet=10ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU..i&w=1000&h=1000&bih=733&biw=1440&q=minnie%20mouse&ved=0ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU&iact=mrc&uact=8"
-    //   scores:[
-    //     1,
-    //     5,
-    //     3,
-    //     2,
-    //     4,
-    //     2,
-    //     1,
-    //     5, 
-    //     3,
-    //     5
-    // ]
+      yourPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.homedepot-static.com%2FproductImages%2Fd8a8d24a-7866-465c-829e-194c9ab87a24%2Fsvn%2Fblue-roommates-wall-decals-rmk1509gm-64_1000.jpg&imgrefurl=https%3A%2F%2Fwww.homedepot.com%2Fp%2FRoomMates-5-in-x-19-in-Mickey-and-Friends-Minnie-Mouse-Peel-and-Stick-Giant-Wall-Decal-8-Piece-RMK1509GM%2F202984854&docid=Ki3lxYdcYd4oaM&tbnid=YdK4YuW-kt-v0M%3A&vet=10ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU..i&w=1000&h=1000&bih=733&biw=1440&q=minnie%20mouse&ved=0ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU&iact=mrc&uact=8",
+      scores:[
+        1,
+        5,
+        3,
+        2,
+        4,
+        2,
+        1,
+        5, 
+        3,
+        5
+    ]
     },
   ];
   
