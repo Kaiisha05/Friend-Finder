d524f0bc6803e881a4b5a55aa2d58969b3f8f83e
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-18 20:41:40 -0500

    api pages loading with built-in friends data array

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -14,7 +14,11 @@ var friendsArray = [
     //     3,
     //     5
     // ]
-    }
+    },
+    {
+      yourName: "Minnie Mouse",
+      yourPhoto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.homedepot-static.com%2FproductImages%2Fd8a8d24a-7866-465c-829e-194c9ab87a24%2Fsvn%2Fblue-roommates-wall-decals-rmk1509gm-64_1000.jpg&imgrefurl=https%3A%2F%2Fwww.homedepot.com%2Fp%2FRoomMates-5-in-x-19-in-Mickey-and-Friends-Minnie-Mouse-Peel-and-Stick-Giant-Wall-Decal-8-Piece-RMK1509GM%2F202984854&docid=Ki3lxYdcYd4oaM&tbnid=YdK4YuW-kt-v0M%3A&vet=10ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU..i&w=1000&h=1000&bih=733&biw=1440&q=minnie%20mouse&ved=0ahUKEwi4sIjzp47nAhWPxFkKHXPoBXkQMwiyASgFMAU&iact=mrc&uact=8"
+    },
   ];
   
 //   // Note how we export the array. This makes it accessible to other files using require.
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,13 +1,7 @@
 //Load Data
 // ======================================================
-var friendsData = require("../data/friends")
+var friendsData = require("../data/friendsData")
 
-// friendsData = [
-//     {
-//         name: "Mickey Mouse",
-//         photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fd%2Fd4%2FMickey_Mouse.png%2F220px-Mickey_Mouse.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMickey_Mouse&docid=o8fKNZd6urBFOM&tbnid=cfGkw9uCLWAi0M%3A&vet=10ahUKEwjWot-It_fmAhVHj1kKHef3CagQMwiXASgAMAA..i&w=220&h=220&bih=731&biw=1440&q=mickey%20mouse&ved=0ahUKEwjWot-It_fmAhVHj1kKHef3CagQMwiXASgAMAA&iact=mrc&uact=8"
-//     }
-// ]
 
 module.exports = function (app) {
 //GET Routing
@@ -15,23 +9,23 @@ module.exports = function (app) {
 app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
-
+console.log("console log #1: ", friendsData);
 
   //POST Routing
   // ======================================================
   //Display matched friend -- will hanlde the compatibiilty logic
   app.post("/api/friends", function (req, res) {
-    // var newFriend = req.body;
+    var newFriend = req.body;
 
     //Using a RegEx Pattern to remove spaces from newFriend
 
     // newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
-    console.log("console #2:" ,friendsData);
+    console.log("console #2:" ,newFriend);
 
-    friendsData.push(req.body);
+    friendsData.push(newFriend);
 
-    res.json(friendsData);
+    res.json(newFriend);
   });
 };
 
