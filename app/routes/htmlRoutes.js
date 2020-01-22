9f4e4ee79d7e5a709f84aa93477d8ba56b946674
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-21 20:47:21 -0500

    added express access to the public folder which allowed us to access the styls.css file. server.js file was outside of the public folder

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -6,6 +6,8 @@ var path = require("path");
 // //ROUTING 
 module.exports = function(app) {
 
+  
+
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
--- server.js
+++ server.js
@@ -15,38 +15,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('../app/public'))
 
-
-// ROUTES - moved to htmlRoutes file
-// ======================================================
-
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "home.html"));
-// });
-//  app.get("/survey", function (req,res) {
-//    res.sendFile(path.join(__dirname, "survey.html"));
-//  });
-
-
-// Display all Friends - moved to apiRoutes
-// ======================================================
-// app.get("/api/friends", function (req,res) {
-//   return res.json(friends);
-// });
-
-// // Display matched friend -- will hanlde the compatibiilty logic
-// app.post("api/friends", function (req, res) {
-//   var newFriend = req.body;
-
-// // Using a RegEx Pattern to remove spaces from newCharacter
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newFriend);
-
-//   friends.push(newFriend);
-  
-//   res.json(newFriend);
-// });
 
 
 // ROUTER - The below points our server to a series of "route" files. These routes give our server a "map" of how to respond when users visit or request data from various URLs.
