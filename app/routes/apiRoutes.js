d3110e03086af48d625c3fb9bcac41f8f41eaa93
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-22 00:34:07 -0500

    matching works

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,34 +1,70 @@
 //Load Data
 // ======================================================
 var friendsData = require("../data/friendsData")
+// linking routes to a series of "data" sources. These data sources hold arrays of information of all possible friends
 
 
 module.exports = function (app) {
-//GET Routing
+
+
+//API GET ROUTING
 // ======================================================
 app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
-console.log("console log #1: ", friendsData);
+  
+// console.log("apiRoutes.js: ", friendsData);
 
-  //POST Routing
-  // ======================================================
-  //Display matched friend -- will hanlde the compatibiilty logic
+  //API POST ROUTING
+  //=====================================================
   app.post("/api/friends", function (req, res) {
-    var newFriend = req.body;
+    
+//Creates an object for the compatible friend.
+    var friendMatch = {
+  yourName: "",
+  yourPhoto: "",
+  friendDifference: Infinity
+};
 
-    //Using a RegEx Pattern to remove spaces from newFriend
+ //When the questionnaire is submitted, the server will respond and provide the results. 
+ var newFriend = req.body;
+ var newFriendScore = newFriend.scores;
 
-    // newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
+//COMPATIBILITY LOGIC - will take the difference between the new friend's and existing friends' scores
+var totalDifference;
 
-    console.log("console #2:" ,newFriend);
+//Iterate through all the existing friends in the databasae
+for (var i = 0; i < friendsData.length; i++) {
+  var currentFriends = friendsData[i];
+  totalDifference = 0;
+console.log("currentUser: ", currentFriends.name);
+
+//Iterate through all the scores of the existing friends. 
+for (var f = 0; f < currentFriends.scores.length; f++) {
+var currentFriendsScore = currentFriends.scores[f];
+var currentnewFriendScore = newFriendScore[f];
+
+//CALCULATIONS ==========================================
+totalDifference += Math.abs(parseInt(currentnewFriendScore) - parseInt(currentFriendsScore));
+}
+ if(totalDifference <= friendMatch.friendDifference) {
+   //reset the friendMatch to be the new friend
+   friendMatch.yourName = currentFriends.yourName;
+   friendMatch.yourPhoto = currentFriends.yourPhoto;
+   friendMatch.friendDifference = totalDifference;
+ }
+
+
+};
+
+// console.log("console #2:" ,newFriend);
 
     friendsData.push(newFriend);
 
-    res.json(newFriend);
+    res.json(friendMatch);
   });
+
 };
-
-
-
-
+//friends = friendsData
+//userData = newFriend
+//friendDifference = difference
